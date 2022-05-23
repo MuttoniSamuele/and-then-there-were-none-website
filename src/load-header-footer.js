ffd88752da9cf35b507ba3ea098704baddc2d664
mutti <imperialvictor15@gmail.com>
2022-05-23 20:57:21 +0200

    Fix header and footer load

--- src/load-header-footer.js
+++ src/load-header-footer.js
@@ -1,7 +1,19 @@
-window.addEventListener("load", async () => {
-  let resp = await fetch("/header.html");
-  document.getElementById("header").innerHTML = await resp.text();
-  resp = await fetch("/footer.html");
-  document.getElementById("footer").innerHTML = await resp.text();
-});
+async function loadHtml(path, parentId) {
+  let resp = await fetch(path);
+  if (resp.status === 200) {
+    document.getElementById(parentId).innerHTML = await resp.text();
+    return true;
+  }
+  return false;
+}
 
+window.addEventListener("load", async () => {
+  const url = window.location.href.split("/");
+  if (url[url.length-2] === "pages") {
+    await loadHtml("../header.html", "header");
+    await loadHtml("../footer.html", "footer");
+  } else {
+    await loadHtml("./header.html", "header");
+    await loadHtml("./footer.html", "footer");  
+  }
+});
