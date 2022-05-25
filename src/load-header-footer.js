2467819ebb6c01fb256e2b7d6066460b1d4a50ed
mutti <imperialvictor15@gmail.com>
2022-05-25 21:33:47 +0200

    Add website content

--- src/load-header-footer.js
+++ src/load-header-footer.js
@@ -1,19 +1,15 @@
 async function loadHtml(path, parentId) {
   let resp = await fetch(path);
   if (resp.status === 200) {
-    document.getElementById(parentId).innerHTML = await resp.text();
-    return true;
+    const parent = document.getElementById(parentId);
+    if (parent) {
+      parent.innerHTML = await resp.text();
+    }
   }
-  return false;
 }
 
 window.addEventListener("load", async () => {
-  const url = window.location.href.split("/");
-  if (url[url.length-2] === "pages") {
-    await loadHtml("../header.html", "header");
-    await loadHtml("../footer.html", "footer");
-  } else {
-    await loadHtml("./header.html", "header");
-    await loadHtml("./footer.html", "footer");  
-  }
+  await loadHtml("header.html", "header");
+  await loadHtml("footer.html", "footer");
+  await loadHtml("trailer.html", "trailer-container");
 });
